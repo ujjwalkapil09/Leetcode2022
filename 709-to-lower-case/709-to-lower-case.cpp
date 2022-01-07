a3ce92104be57c779f71958d39fb5658481b6be2
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-01-07 22:09:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (51.17%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string ans;
+        for(int i=0;i<s.length();i++){
+            if(s[i]>=65 && s[i]<=90){
+                ans+=s[i]+32;
+            }
+            else{
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
