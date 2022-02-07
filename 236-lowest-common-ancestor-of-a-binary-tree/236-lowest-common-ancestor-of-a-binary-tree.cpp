4b53cf7f0d4764c46a567981317d9b9794e9b714
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-02-07 11:02:47 +0530

    Time: 20 ms (55.85%), Space: 14.2 MB (86.72%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,64 +9,20 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> first;
-    vector<TreeNode *>second;
-    int check;
-    int scheck;
-    bool common(TreeNode *i){
-       if(!i) return false;
-        first.push_back(i);
-        if(i->val==check){
-            return true;
-        }
-        if(common(i->left) || common(i->right)){
-            return true;
-        }
-        first.pop_back();
-        return false;
-    }
-    
-    bool Scommon(TreeNode *i){
-       if(!i) return false;
-        second.push_back(i);
-        if(i->val==scheck){
-            return true;
-        }
-        if(Scommon(i->left) || Scommon(i->right)){
-            return true;
-        }
-        second.pop_back();
-        return false;
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        check=p->val;
-        TreeNode *o=root;
-        // second.push_back(root);
-        scheck=q->val;
-        common(root);
-        Scommon(o);
-        reverse(first.begin(),first.end());
-        reverse(second.begin(),second.end());
-
-        vector<TreeNode*> size;
-        vector<TreeNode*> small;
-        if(first.size()<second.size()){
-            size=first;
-            small=second;}
+        if(root==NULL || root==p || root==q){
+            return root;
+        }
+        TreeNode *left= lowestCommonAncestor(root->left,p,q);
+        TreeNode *right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL){
+            return right;
+        }
+        else if(right==NULL){
+            return left;
+        }
         else{
-            size=second;
-            small=first;
+            return root;
         }
-        for(int i=0;i<size.size();i++){
-            for(int j=0;j<small.size();j++){
-                if(size[i]==small[j]){
-                    return small[j];
-                }
-            }
-        }
-        return NULL;
-        
-        
     }
 };
