3b04737c5aa796b9b9b18e62b3112a34062d8e6c
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-03-17 16:56:57 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (90.80%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int arr[3]={0,0,0};
+        for(int i=0;i<nums.size();i++){
+            arr[nums[i]]++;
+        }
+        int i=0, j=0;
+        while(i<3){
+            if(arr[i]==0)i++;
+            else{
+                nums[j++]=i;
+                arr[i]--;
+            }
+        }
+        
+    }
+};
