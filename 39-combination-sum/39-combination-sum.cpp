6d592d2eed8043a57123bcad66ddcf57f253146b
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-07-14 13:13:07 +0530

    Time: 8 ms (80.94%), Space: 11.1 MB (68.79%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void findCom(int ind ,int target,vector<int>&arr, vector<vector<int>> &ans,vector<int> &ds){
+        if(ind==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        //pick
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            findCom(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+        }
+        findCom(ind+1,target,arr,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCom(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
