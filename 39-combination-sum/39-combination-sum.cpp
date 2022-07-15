99a58635d48fe0ab6ceb2c516d4cce1b88497610
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-07-15 12:49:36 +0530

    Time: 11 ms (69.00%), Space: 11.2 MB (56.53%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     void findCom(int ind ,int target,vector<int>&arr, vector<vector<int>> &ans,vector<int> &ds){
+        if(target==0){
+            ans.push_back(ds);
+            return ;
+        }
         if(ind==arr.size()){
             if(target==0){
                 ans.push_back(ds);
             }
+            return ;
+        }
+        if(target==0){
+            ans.push_back(ds);
             return ;
         }
         //pick
