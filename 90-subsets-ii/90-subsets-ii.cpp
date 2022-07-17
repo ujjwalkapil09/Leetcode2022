789676ce66499704e0fdad93ee74780037f4dd75
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-07-17 13:40:24 +0530

    Time: 7 ms (49.26%), Space: 7.4 MB (89.62%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void Subset_non(set<vector<int>> &answer,vector<int> &temp,vector<int>arr,int ind){
-        if(ind==arr.size()){
-            answer.insert(temp);
-            return ;
+    void find(int ind,vector<int> &nums,vector<int> &ds,
+             vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind &&nums[i]==nums[i-1])continue;
+            ds.push_back(nums[i]);
+            find(i+1,nums,ds,ans);
+            ds.pop_back();
         }
-        temp.push_back(arr[ind]);
-        Subset_non(answer,temp,arr,ind+1);
-        temp.pop_back();
-        Subset_non(answer,temp,arr,ind+1);
-        return ;
+        
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
         sort(nums.begin(),nums.end());
-        vector<vector<int>> Fans;
-        set<vector<int>> answer;
-        vector<int> temp;
-        Subset_non(answer,temp,nums,0);
-        for(auto itr: answer){
-            Fans.push_back(itr);
-        }
-        return Fans;
+        find(0,nums,ds,ans);
+        return ans;
     }
 };
