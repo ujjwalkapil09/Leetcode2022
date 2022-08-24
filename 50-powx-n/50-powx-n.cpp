9c417ddc19579f590b1417f90ba7fb225744ff66
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-08-24 21:24:44 +0530

    Time: 4 ms (33.63%), Space: 5.8 MB (96.17%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long nn=n;
+        double ans=1.0;
+        if(n<0){
+            nn=(-1)*nn;
+        }
+        while(nn){
+            if(nn % 2){
+                ans=(double)ans*x;
+                nn=nn-1;
+                
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+        cout<<ans;
+        if(n<0){
+            ans=(double)1.0/(double)ans;
+        }
+        return ans;
     }
 };
