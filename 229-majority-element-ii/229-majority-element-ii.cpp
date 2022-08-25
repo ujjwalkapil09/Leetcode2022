2613a4cc96b6d0d6b89dd51095eae30c982e2d73
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-08-25 20:44:51 +0530

    Time: 9 ms (96.96%), Space: 15.9 MB (52.94%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int s=floor(nums.size()/3);
-        sort(nums.begin(),nums.end());
-        int last_dup=nums[0];
-        int count=1;
-        // cout<<s<<"  ";
-        // if()
-        vector<int> ans;
-        for(int i=1;i<nums.size();i++){
-            
-            if(last_dup!=nums[i]){
-                // cout<<"\n"<<count;
-                if(s<count){
-                ans.push_back(last_dup);}
-                last_dup=nums[i];
-                
-            
-                count=1;
-                
-                // continue;
+        int sz=nums.size();
+        int num1=-1,num2=-1;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<sz;i++){
+            if(nums[i]==num1){
+                count1++;
             }
-            
-            else if(last_dup==nums[i]){
-                // cout<<"r"<<nums[i];
-                count++;
+            else if(nums[i]==num2){
+                count2++;
+            }
+            else if(count1==0){
+                num1=nums[i];
+                count1=1;
                 
+            }
+            else if(count2==0){
+                num2=nums[i];
+                count2=1;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
-        if(s<count){
-                ans.push_back(last_dup);}
+        vector<int> ans;
+        count1=count2=0;
+        // cout<<num2<<" ";
+        for(auto it: nums){
+            if(num1==it){
+                count1++;
+            }
+            else if(num2==it){
+                count2++;
+            }
+        }
+        // cout<<count1<<" "<<count2;
+        if(count1>floor(sz/3)){
+            ans.push_back(num1);
+        }
+        if(count2>floor(sz/3)){
+            ans.push_back(num2);
+        }
         return ans;
-        
     }
 };
