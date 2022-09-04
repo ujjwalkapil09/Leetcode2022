4ca7b84373bb07ff667d6971ae5c8619c483de81
Ujjwal Kapil <89023734+ujjwalkapil09@users.noreply.github.com>
2022-09-04 17:15:25 +0530

    Time: 7 ms (92.95%), Space: 7.5 MB (90.18%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -1,122 +1,33 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
-class Solution {
-public:
-//     int length(ListNode *i){
-//         int len=0;
-//         while(i){
-//             len++;
-//             i=i->next;
-//         }
-//         return len;
-        
-//     }
-    int op;
-    void detect(ListNode* p){
-        ListNode*a=p,*u=p,*aa=p;
-        int count=0;
-        do{
-            // cout<<"test"<<"\t";
-            u=u->next;
-            count++;
-            a=a->next;
-            if(a->next!=NULL){
-                a=a->next;
-            }
-            if(a==u)break;
-        }while(u && a && a!=u);
-//         cout<<u->val<<"  "<<a->val<<"after while";
-//         // cout<<count<<" _ ";
-//         if(u==a){
-//             if(count==2){cout<<"tail connects to node index ";
-//             op= u->val;
-//             return;  }
-//             if(count==1){
-//                 cout<<"no cycle";
-//                         op= -111111;
-//             return ;
-//             }
-//             a=a->next;
-//             // u=u->next;
-//             cout<<"tail connects to node index ";
-//             op= a->val;
-//             return;
-            
-//         }
-//         else{
-            // cout<<"no cycle";
-            //             op= -111111;
-            // return ;
+class Solution
+{
+    public:
+        ListNode* detectCycle(ListNode *head)
+        {
+            if (head == NULL || head->next == NULL) return NULL;
+            ListNode *slow = head, *fast = head, *entry = head;
+            while (fast->next && fast->next->next)
+            {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast)
+                {
+                    while (slow != entry)
+                    {
 
-//         }
-        do{
-            if(aa==a){
-                break;
+                        slow = slow->next;
+                        entry = entry->next;
+                    }
+                    return entry;
+                }
             }
-            aa=aa->next;
-            a=a->next;
-        }while(a && aa && a!=aa);
-        // op=aa->val;
-        cout<<aa->val;
-        if(aa==a){
-            // cout<<"tail connects to node index ";
-            op= a->val;
-            return;
-        }
-        else{
-            // cout<<"no cycle";
-                        op= -111111;
-            return ;
-        }
-    }
-    ListNode *detectCycle(ListNode *head) {
-        ListNode *check=head,*p=head;
-        if(head==NULL){
-            // cout<<"no cycle";
             return NULL;
         }
-        if(head->next==check){
-                        // cout<<"tail connects to node index ";
-        return check;
-        }
-        if(head->next==NULL){
-            
-            cout<<"no cycle";
-            return NULL;
-        }
-        ListNode *a=p,*u=p,*aa=p;
-        // int count=0;
-        do{
-            // cout<<"test"<<"\t";
-            u=u->next;
-            // count++;
-            a=a->next;
-            if(a==NULL)break;
-            if(a->next!=NULL){
-                a=a->next;
-            }
-            else{
-                break;
-            }
-            if(a==u)break;
-        }while(u && a && a!=u);
-        do{
-            if(aa==a){
-                break;
-            }
-            // if(a)
-            aa=aa->next;
-                        if(a==NULL)break;
-
-            a=a->next;
-        }while(a && aa && a!=aa);
-        return a;
-        return NULL;
-    }
 };
